024a6a94b294eeeaed77e5c3740c776e0fa36e64
Christopher Kok <ForceLightning@users.noreply.github.com>
2023-09-24 18:48:30 +0800

    removed unused lines, steganography class should work now

--- main.py
+++ main.py
@@ -5,26 +5,16 @@ from tkinter import *
 from tkinter.ttk import *
 from tkinter import filedialog
 
-#from steganography.steganography import Steganography
-from steganography.decoder import ImageDecoder, AudioDecoder, VideoDecoder
-from steganography.encoder import ImageEncoder, AudioEncoder, VideoEncoder
+from steganography.steganography import Steganography
 
-
-# Initialize Steganography and Decoders
-#stega = Steganography()
-image_decoder = ImageDecoder()
-audio_decoder = AudioDecoder()
-video_decoder = VideoDecoder()
-image_encoder = ImageEncoder()
-audio_encoder = AudioEncoder()
-video_encoder = VideoEncoder()
 
 def main():
     root = Tk()
     root.title("Steganography")
     root.geometry("500x500")
     root.resizable(False, False)
-    #stega = Steganography()
+    # Initialize Steganography
+    stega = Steganography()
 
     # TODO: Add and initialise widgets here
 
@@ -41,17 +31,16 @@ def main():
     browse_button.pack()
 
     # TODO(Widgets): Add drag-and-drop functionality to select cover, encoded, and/or output files.
-    
+
 
     # TODO(Widgets): Setup button to encode secret message into cover file.
     # ! use stega.encode(cover_file, secret_data, output_file)
-   
+
 
     # TODO(Widgets): Setup button to decode secret message from encoded file.
     # ! use stega.decode(encoded_file)
-    
 
-    
+
     # TODO(Stega/Encoder): Encode secret message in text box into cover file and save to output file
 
     # TODO(Stega/Decoder): Display decoded secret message in text box
