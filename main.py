e0833eaf42fbad5f2b2aec5f0acb5f976b2a4e85
He Zexi <hezexi0616@gmail.com>
2023-09-25 16:39:48 +0800

    UPDATE MAIN.PY WITH IMAGE PREVIEW

--- main.py
+++ main.py
@@ -3,38 +3,56 @@ import sys
 
 from tkinter import *
 from tkinter.ttk import *
-
-from steganography import Steganography
-
+from tkinter import filedialog
+from tkinter import messagebox
+from PIL import Image, ImageTk
+import pygame
+from moviepy.editor import VideoFileClip
+import threading
 
 def main():
+    pygame.mixer.init()
+    pygame.font.init()
+
     root = Tk()
     root.title("Steganography")
     root.geometry("500x500")
     root.resizable(False, False)
-    stega = Steganography()
-    # TODO: Add and initialise widgets here
 
-    # TODO(Widgets): Setup text input box for secret message
-    # TODO(Widgets): Setup button to open file dialog to select cover, encoded, and/or output files.
-    # TODO(Widgets): Add drag-and-drop functionality to select cover, encoded, and/or output files.
+    open_button = Button(root, text="Open File", command=lambda: open_file_dialog(root))
+    open_button.pack()
 
-    # TODO(Widgets): Setup button to encode secret message into cover file.
-    # ! use stega.encode(cover_file, secret_data, output_file)
-
-    # TODO(Widgets): Setup button to decode secret message from encoded file.
-    # ! use stega.decode(encoded_file)
-
-    # TODO(Stega/Encoder): Encode secret message in text box into cover file and save to output file
-    
-    # TODO(Stega/Decoder): Display decoded secret message in text box
-
-    # TODO(Preview): Display/play cover image/audio/video in main window (or in a new window)
-    # ! maybe use system default image/audio/video viewer/player, write to a temporary file, or use a library like PIL, OpenCV, or PyGame
-    
     root.mainloop()
-    pass
 
+def open_file_dialog(root):
+    file_path = filedialog.askopenfilename()
+    if file_path:
+        file_extension = file_path.split('.')[-1].lower()
+        if file_extension in ('jpg', 'jpeg', 'png', 'gif'):
+            # Preview Image
+            image = Image.open(file_path)
+            image.thumbnail((400, 400))
+            photo = ImageTk.PhotoImage(image)
+            img_label = Label(root, image=photo)
+            img_label.photo = photo
+            img_label.pack()
+        elif file_extension in ('mp3', 'wav'):
+            # Play Audio
+            pygame.mixer.music.load(file_path)
+            pygame.mixer.music.play()
+        elif file_extension in ('mp4', 'avi', 'mov'):
+            # Play Video
+            video_thread = threading.Thread(target=play_video, args=(root, file_path))
+            video_thread.start()
+        else:
+            messagebox.showinfo("Unsupported Format", "Unsupported file format for preview.")
+
+def play_video(root, file_path):
+    try:
+        video_clip = VideoFileClip(file_path)
+        video_clip.preview(fps=24, audio=True, threaded=True, winname="Video Preview")
+    except Exception as e:
+        messagebox.showerror("Error", f"Error playing video: {str(e)}")
 
 if __name__ == "__main__":
     main()
