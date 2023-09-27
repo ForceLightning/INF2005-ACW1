26113e621a87bd0493e5ee81afadf028f8943182
Christopher Kok <ForceLightning@users.noreply.github.com>
2023-09-27 08:30:09 +0800

    feat: added video preview frame feat: added working encode/decode + LSB selection button feat: added working decode loading button

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ import os
 import sys
 import threading
 
+from typing import Union
 from tkinter import *
 from tkinter.ttk import *
 from tkinter import filedialog, messagebox
 from PIL import Image, ImageTk
+import cv2
+from numpy import place
 import pygame
 from moviepy.editor import VideoFileClip
 from tkinterdnd2 import TkinterDnD, DND_FILES
+from steganography.encoder import AudioEncoder, ImageEncoder, VideoEncoder
 
 from steganography.steganography import Steganography
 from steganography.util import IMAGE_EXTENSIONS, AUDIO_EXTENSIONS, VIDEO_EXTENSIONS
@@ -19,6 +23,7 @@ from steganography.util import IMAGE_EXTENSIONS, AUDIO_EXTENSIONS, VIDEO_EXTENSI
 # image used <a href="https://www.flaticon.com/free-icons/equalizer" title="equalizer icons">Equalizer icons created by Ehtisham Abid - Flaticon</a>
 # Initialize Steganography and Decoders
 stega = Steganography()
+temp_path = None
 
 # TODO(Zibin): Create the layout as discussed
 
@@ -31,71 +36,96 @@ stega = Steganography()
 # TODO(Yok): Finish the project
 
 # Function to handle drop event
+
+
 def drop(event):
     global dropped_file_path
     dropped_file_path = event.data.strip("{}")
-    print(f"File Path: {dropped_file_path}") # For debug
+    print(f"File Path: {dropped_file_path}")  # For debug
     process_file(dropped_file_path, is_dropped=True)
 
-def display_image(label, file_path):
-    image = Image.open(file_path)
+
+def display_image(label, image: Union[str, Image.Image]):
+    if isinstance(image, str):
+        image = Image.open(image)
     # Limit the size of the displayed image
     image.thumbnail((200, 200))
     photo = ImageTk.PhotoImage(image)
     label.config(image=photo)
     label.photo = photo
 
-def process_file(file_path, is_dropped=False):
 
+def process_file(file_path, placeholder_image_location, encode=True):
     if file_path:
-        file_extension = file_path.split('.')[-1].lower()
+        file_extension = os.path.splitext(file_path)[1][1:].lower()
         print(f"File Extension: {file_extension}")
         if file_extension in IMAGE_EXTENSIONS:
-            if not is_dropped:
-                # Display Before Image
-                display_image(before_image, file_path)
-                # Encode and Display After Image
-                secret_message = secret_message_entry.get("1.0", 'end-1c')
-                # TODO(GUI): Use the selected number of LSBs
-                encoded_image_path = encode_image(file_path, secret_message)
-                global after_image_pil  # Declare it as global to update it
-                after_image_pil = Image.open(encoded_image_path)  # Update after_image_pil
-                display_image(after_image, encoded_image_path)
-            else:
-                # Display Dropped Image
-                display_image(dropped_image, file_path)
+            display_image(placeholder_image_location, file_path)
         elif file_extension in AUDIO_EXTENSIONS:
             # Play Audio
-            display_image(before_image, "tests/audioimage.png")
-            play_file_button_encode = Button(encode_button_frame, text="Play File", command=lambda: play_audio(file_path))
-            play_file_button_encode.grid(row=0, column=1)
+            display_image(placeholder_image_location, "tests/audioimage.png")
+            if encode:
+                play_file_button_encode = Button(
+                    encode_button_frame, text="Play File", command=lambda: play_audio(file_path))
+                play_file_button_encode.grid(row=0, column=1)
+            else:
+                play_file_button_decode = Button(
+                    decode_button_frame, text="Play File", command=lambda: play_audio(file_path))
+                play_file_button_decode.grid(row=0, column=1)
             # play_audio(file_path)
         elif file_extension in VIDEO_EXTENSIONS:
-            # TODO(GUI): Add a preview frame of the video like the images
-            # ! and provide some indication that a file has been loaded.
-            # Play Video
-            play_video(root, file_path)
+            video = cv2.VideoCapture(file_path)
+            ret, frame = video.read()
+            if ret:
+                # Display Before Image
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                pil_image = Image.fromarray(frame)
+                display_image(placeholder_image_location, pil_image)
+                if encode:
+                    play_file_button_encode = Button(
+                        encode_button_frame, text="Play File", command=lambda: play_video(root, file_path))
+                    play_file_button_encode.grid(row=0, column=1)
+                else:
+                    play_file_button_decode = Button(
+                        decode_button_frame, text="Play File", command=lambda: play_video(root, file_path))
+                    play_file_button_decode.grid(row=0, column=1)
         else:
-            messagebox.showinfo("Unsupported Format", "Unsupported file format for preview.")
+            messagebox.showinfo("Unsupported Format",
+                                "Unsupported file format for preview.")
+
 
 def browse_file():
-    file_path = filedialog.askopenfilename()
-    process_file(file_path)
+    global dropped_file_path, before_image, play_file_button_encode, encode_button_frame
+    dropped_file_path = filedialog.askopenfilename()
+    process_file(dropped_file_path, before_image)
+
+
+def browse_file_decode():
+    global after_image_path, after_image, play_file_button_decode, decode_button_frame
+    after_image_path = filedialog.askopenfilename()
+    if after_image_path:
+        process_file(after_image_path, after_image, False)
 
 # Function to encode image
 def encode_image(file_path, secret_message, save_path=None, num_lsb=1):
+    global after_image_path
     # If a save path is provided, use it; otherwise, create a new file name
-    encoded_image_path = save_path if save_path else file_path.replace(".", "_encoded.")
-    stega.encode(file_path, secret_message, encoded_image_path, num_lsb)
-    return encoded_image_path
+    after_image_path = stega.encode(file_path, secret_message, True, num_lsb)
+    return after_image_path
 
 # Function to decode image
+
+
 def decode_image():
-    global dropped_file_path 
-    if dropped_file_path:
+    global after_image_path
+    if after_image_path:
         try:
-            decoded_message = stega.decode(dropped_file_path)
-            messagebox.showinfo("Decoded Message", f"The decoded message is: {decoded_message}")
+            num_lsb = int(lsb_combobox.get())
+            decoded_message = stega.decode(after_image_path, num_lsb)
+            # messagebox.showinfo("Decoded Message",
+            #                     f"The decoded message is: {decoded_message}")
+            secret_message_entry.delete("1.0", END)
+            secret_message_entry.insert("1.0", decoded_message)
         except Exception as e:
             messagebox.showerror("Error", f"Error decoding message: {str(e)}")
     else:
@@ -112,45 +142,85 @@ def encode_av(file_path, secret_message, output_path):
 def save_encoded_file():
     global dropped_file_path, after_image_pil
     num_lsb = int(lsb_combobox.get())
+    secret_message = secret_message_entry.get("1.0", 'end-1c')
     if after_image_pil:
-        save_path = filedialog.asksaveasfilename(defaultextension=".png", filetypes=[("PNG files", "*.png")])
+        save_path = filedialog.asksaveasfilename(
+            defaultextension=".bmp", filetypes=[("BMP files", "*.BMP")])
         if save_path:
             after_image_pil.save(save_path)
             messagebox.showinfo("Success", "After Image saved successfully.")
     elif dropped_file_path:
         file_extension = dropped_file_path.split('.')[-1].lower()
         if file_extension in IMAGE_EXTENSIONS:
-            save_path = filedialog.asksaveasfilename(defaultextension=".png", filetypes=[("PNG files", "*.png")])
-            if save_path:
-                secret_message = secret_message_entry.get()
-                # Encode the image and save it to the specified path
-                encode_image(dropped_file_path, secret_message, save_path, num_lsb)
-                messagebox.showinfo("Success", "Encoded image saved successfully.")
+            save_path = filedialog.asksaveasfilename(
+                defaultextension=".bmp", filetypes=[("BMP files", "*.bmp")])
         elif file_extension in AUDIO_EXTENSIONS:
-            save_path = filedialog.asksaveasfilename(defaultextension=".wav", filetypes=[("WAV files", "*.wav")])
-            if save_path:
-                secret_message = secret_message_entry.get()
-                encode_av(dropped_file_path, secret_message, save_path)
-                messagebox.showinfo("Success", "Encoded audio saved successfully.")
+            save_path = filedialog.asksaveasfilename(
+                defaultextension=".wav", filetypes=[("WAV files", "*.wav")])
         elif file_extension in VIDEO_EXTENSIONS:
-            save_path = filedialog.asksaveasfilename(defaultextension=".mov", filetypes=[("MOV files", "*.mov")])
-            if save_path:
-                secret_message = secret_message_entry.get()
-                encode_av(dropped_file_path, secret_message, save_path)
-                messagebox.showinfo("Success", "Encoded video saved successfully.")
+            save_path = filedialog.asksaveasfilename(
+                defaultextension=".mov", filetypes=[("MOV files", "*.mov")])
+        else:
+            messagebox.showerror("Error", "Unsupported file format.")
+            return
+        if save_path:
+            try:
+                stega.encode(dropped_file_path, secret_message,
+                             save_path, num_lsb)
+            except Exception as e:
+                messagebox.showerror("Error", f"Error encoding file: {str(e)}")
     else:
         messagebox.showwarning("No File", "No dropped file to encode.")
 
+
+def encode_file():
+    global dropped_file_path, after_image_pil, temp_path, play_file_button_decode, decode_button_frame, after_image_path
+    if dropped_file_path:
+        secret_message = secret_message_entry.get("1.0", 'end-1c')
+        if not secret_message:
+            messagebox.showwarning(
+                "No Message", "No secret message to encode.")
+            return
+        if not dropped_file_path:
+            messagebox.showwarning("No File", "No dropped file to encode.")
+            return
+        num_lsb = int(lsb_combobox.get())
+        try:
+            temp_path = stega.encode(
+                dropped_file_path, secret_message, True, num_lsb)
+            after_image_path = temp_path
+            match stega.encoder:
+                case ImageEncoder():
+                    display_image(after_image, temp_path)
+                case AudioEncoder():
+                    display_image(after_image, "tests/audioimage.png")
+                    play_file_button_decode = Button(
+                        decode_button_frame, text="Play File", command=lambda: play_audio(temp_path))
+                case VideoEncoder():
+                    video = cv2.VideoCapture(temp_path)
+                    ret, frame = video.read()
+                    if ret:
+                        # Display after Image
+                        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                        after_image_pil = Image.fromarray(frame)
+                        display_image(after_image, after_image_pil)
+                    play_file_button_decode = Button(
+                        decode_button_frame, text="Play File", command=lambda: play_video(root, temp_path))
+        except Exception as e:
+            messagebox.showerror("Error", f"Error encoding file: {str(e)}")
+
 # Clear all file
 def clear_images():
-    global dropped_file_path, after_image_pil
+    global dropped_file_path, after_image_pil, after_image_path
     # Clear the images
     before_image.config(image='')
     after_image.config(image='')
     dropped_image.config(image='')
     # Reset the global variables
     dropped_file_path = None
+    after_image_path = None
     after_image_pil = None
+
 
 def play_audio(file_path):
     def audio_thread():
@@ -166,9 +236,12 @@ def play_audio(file_path):
     audio_playback_thread = threading.Thread(target=audio_thread)
     audio_playback_thread.start()
 
+
 def play_video(root, file_path):
-    video_thread = threading.Thread(target=play_video_clip, args=(root, file_path))
+    video_thread = threading.Thread(
+        target=play_video_clip, args=(root, file_path))
     video_thread.start()
+
 
 def play_video_clip(root, file_path):
     try:
@@ -180,18 +253,20 @@ def play_video_clip(root, file_path):
     except Exception as e:
         messagebox.showerror("Error", f"Error playing video: {str(e)}")
 
+
 dropped_file_path = None
 after_image_pil = None
+after_image_path = None
 
 
 def main():
-    global before_image, after_image, dropped_image, secret_message_entry, lsb_combobox, root,encode_frame,encode_button_frame
+    global before_image, after_image, dropped_image, secret_message_entry, lsb_combobox, root, encode_frame, encode_button_frame, decode_frame, decode_button_frame, after_image_path
     pygame.mixer.init()
     pygame.font.init()
 
     root = TkinterDnD.Tk()
     root.title("Steganography")
-    root.geometry("900x450")
+    root.geometry("835x700")
     root.resizable(False, False)
 
     # Encode Frame #
@@ -206,16 +281,16 @@ def main():
     encode_button_frame.pack()
 
     # Encode buttons
-    load_file_button_encode = Button(encode_button_frame, text="Load File", command=browse_file)
+    load_file_button_encode = Button(
+        encode_button_frame, text="Load File", command=browse_file)
     load_file_button_encode.grid(row=0, column=0)
-    
+
     # play_file_button_encode = Button(encode_button_frame, text="Play File", command=lambda: play_file(file_path))
     # play_file_button_encode.grid(row=0, column=1)
 
     before_image = Label(encode_frame)
     before_image.pack()
 
-    
     # Decode Frame #
     decode_frame = Frame(root)
     decode_frame.grid(row=0, column=1, padx=10, pady=10)
@@ -228,19 +303,19 @@ def main():
     decode_button_frame.pack()
 
     # Decode buttons
-    load_file_button_decode = Button(decode_button_frame, text="Load File")
+    load_file_button_decode = Button(decode_button_frame, text="Load File", command=browse_file_decode)
     load_file_button_decode.grid(row=0, column=0)
 
     play_file_button_decode = Button(decode_button_frame, text="Play File")
     play_file_button_decode.grid(row=0, column=1)
 
-    save_file_button = Button(decode_button_frame, text="Save File", command=save_encoded_file)
+    save_file_button = Button(
+        decode_button_frame, text="Save File", command=save_encoded_file)
     save_file_button.grid(row=0, column=2)
 
     after_image = Label(decode_frame)
     after_image.pack()
 
-    
     # Secret Message Frame #
     secret_message_frame = Frame(root)
     secret_message_frame.grid(row=1, columnspan=2, padx=50)
@@ -252,13 +327,13 @@ def main():
     action_button_frame = Frame(root)
     action_button_frame.grid(row=2, columnspan=2)
 
-    encode_button = Button(action_button_frame, text="Encode")
+    encode_button = Button(action_button_frame,
+                           text="Encode", command=encode_file)
     encode_button.grid(row=0, column=0, padx=5, pady=5)
 
-    decode_button = Button(action_button_frame, text="Decode", command=decode_image)
+    decode_button = Button(action_button_frame,
+                           text="Decode", command=decode_image)
     decode_button.grid(row=0, column=1, pady=5)
-
-
 
     # global before_image, after_image, dropped_image, secret_message_entry, root, lsb_combobox
     # pygame.mixer.init()
@@ -268,8 +343,6 @@ def main():
     # root.title("Steganography")
     # root.geometry("1200x900")  # Increased height to accommodate the new frame
     # root.resizable(False, False)
-
-
 
     # # Create a frame to hold the before and after images
     # frame = Frame(root)
@@ -291,7 +364,7 @@ def main():
 
     # # Button for adding files to show on before image
     # browse_button = Button(button_frame, text="Select File To Encode", command=browse_file)
-    # browse_button.grid(row=0, column=0, padx=10) 
+    # browse_button.grid(row=0, column=0, padx=10)
 
     # # Button to clear the images
     # clear_button = Button(button_frame, text="Clear Images", command=clear_images)
@@ -319,13 +392,14 @@ def main():
     # secret_message_entry = Entry(root, width=50)
     # secret_message_entry.pack()
 
-    # # Dropdown to choose number of LSBs.
-    # lsb_label = Label(root, text="Select Number of LSBs:")
-    # lsb_label.pack()
-    # lsb_combobox = Combobox(root, values=[1, 2, 3, 4, 5, 6, 7, 8])
-    # lsb_combobox['state'] = 'readonly'
-    # lsb_combobox.current(0) # Default to 1
-    # lsb_combobox.pack()
+    # Dropdown to choose number of LSBs.
+    lsb_label = Label(action_button_frame, text="Select Number of LSBs:")
+    lsb_label.grid(row=0, column=2, pady=5)
+    lsb_combobox = Combobox(action_button_frame, values=[
+                            1, 2, 3, 4, 5, 6, 7, 8])
+    lsb_combobox['state'] = 'readonly'
+    lsb_combobox.current(0)  # Default to 1
+    lsb_combobox.grid(row=0, column=3, pady=5)
 
     # # Button to decode the secret message.
     # decode_button = Button(root, text="Decode Message", command=decode_image)
@@ -335,8 +409,8 @@ def main():
     # save_button = Button(root, text="Save Encoded File", command=save_encoded_file)
     # save_button.pack()
 
-
     root.mainloop()
+
 
 if __name__ == "__main__":
     main()
